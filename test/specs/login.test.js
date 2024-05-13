f322a89f75cd22475ef9d8a74eaf6af9608d8a00
JoaoKleinkauf <jprkleinkauf@gmail.com>
2024-05-13 10:12:18 -0300

    Testes

---
+++ test/specs/login.test.js
@@ -0,0 +1,15 @@
+import {expect, driver} from '@wdio/globals'
+import homePage from '../pageobjects/home.page.js'
+import loginPage from '../pageobjects/login.page.js'
+import profilePage from '../pageobjects/profile.page.js'
+
+describe('My Login application', () => {
+
+    it('should create a new user', async () => {
+        await homePage.openMenu('profile')
+        await homePage.openForm('Sign up')
+        await loginPage.login('João', 'Pedro', '11976413698', 'test@test.com.br', '123456', '123456')
+        await homePage.openMenu('profile')
+        expect ((await profilePage.profileName('João Pedro')).isDisplayed()).toBeTruthy
+    })
+})
